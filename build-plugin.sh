85d0f58aa309fff6b00d11871f4ef63d1020de11
Daniel García Moreno <dani@danigm.net>
2021-03-17 12:19:42 +0100

    Use different Dockerfile for master/stable

--- build-plugin.sh
+++ build-plugin.sh
@@ -9,13 +9,14 @@ nodeenv -p --node=10.17.0
 npm install -g yarn@1.22.10
 yarn global add @vue/cli
 
-# TODO: build custom channel apps
-# git clone https://github.com/endlessm/kolibri-channel-custom-web-app.git
-# cd kolibri-channel-custom-web-app
-# yarn install
-# yarn build
-# yarn deploy ../kolibri-explore-plugin/kolibri_explore_plugin/apps
-# cd ..
+# building all custom channels and copy to kolibri-explore-plugin
+git clone https://github.com/endlessm/kolibri-channel-custom-web-app.git
+export VUE_APP_USE_MOCK_DATA=false
+cd kolibri-channel-custom-web-app
+yarn install
+yarn build
+yarn deploy ../kolibri-explore-plugin/kolibri_explore_plugin/apps
+cd ..
 
 cd kolibri-explore-plugin
 pip install -r requirements.txt
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# update kolibri-explore-plugin
-if [ $EXPLORE_VERSION = "stable" ]
-then
-  pip install -U kolibri-explore-plugin
-else
-  ./build-plugin.sh
-  pip install -f file://$PWD/kolibri-explore-plugin/dist kolibri-explore-plugin
-fi
-
-kolibri plugin enable kolibri.plugins.app
-kolibri plugin disable kolibri.plugins.learn
-kolibri plugin enable kolibri_explore_plugin
-
 if [ $POPULATE_CONTENT = "true" ]
 then
   ./populate.sh
